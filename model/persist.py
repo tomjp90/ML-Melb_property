333604defe6e86f8c528b547622a63952038c24e
Tom Peddlesden <72062388+tomjp90@users.noreply.github.com>
2021-06-14 20:25:41 +1000

    added a new model without crime

--- model/persist.py
+++ model/persist.py
@@ -9,12 +9,24 @@ def load_model():
 
       return load(MODEL_PATH)
 
-
+# no landsize model
 MODEL_PATH_NL = "model/models/model_without_landsize.joblib"
 
 def load_model_NL():
       
       return load(MODEL_PATH_NL)
+
+# no crime model
+MODEL_PATH_NC = "model/models/model_no_crime.joblib"
+
+def load_model_NC():
+      
+      return load(MODEL_PATH_NC)
+
+
+
+
+      
 
 #-------------------- PREDICTION FUNCTIONS -----------------------------
 def predict_value(X):
